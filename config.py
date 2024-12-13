f7924fd2a5b0acb69c2a3492e8296df1995f5302
gaetangomez <gatou09000@gmail.com>
2024-12-13 13:45:55 +0100

    update
    add gitignore and update

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 # Récupère le token depuis les variables d'environnement
-TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+TOKEN = os.getenv("discord_token")
 
 if not TOKEN:
     raise ValueError("Le token du bot n'a pas été configuré dans les variables d'environnement.")
